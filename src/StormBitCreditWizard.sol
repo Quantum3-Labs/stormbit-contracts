c07c7caff0a139b8ba958a7c0988cd346091087e
mrhouzlane <rhouzm@gmail.com>
2024-02-25 15:50:27 +0100

    credit wizzard added

---
+++ src/StormBitCreditWizard.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.21;
+
+import {FunctionsClient} from "@chainlink/contracts/src/v0.8/functions/dev/v1_0_0/FunctionsClient.sol";
+import {ConfirmedOwner} from "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
+import {FunctionsRequest} from "@chainlink/contracts/src/v0.8/functions/dev/v1_0_0/libraries/FunctionsRequest.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract StormBitCreditWizard is FunctionsClient {
+    mapping(address => uint256) public finalScore;
+
+    // ---- CHAINLINK ----
+    using FunctionsRequest for FunctionsRequest.Request;
+
+    string postRequestSrc;
+    uint64 subscriptionId;
+
+    bytes32 public latestRequestId;
+    bytes public latestResponse;
+    bytes public latestError;
+
+    event OCRResponse(bytes32 indexed requestId, bytes result, bytes err);
+    // ---- --------- ----
+
+    uint256 distributionCount = 0;
+
+    constructor(address _functionsRouter, uint64 _subscriptionId) FunctionsClient(_functionsRouter) {
+        subscriptionId = _subscriptionId;
+    }
+
+    function setPostRequestSrc(string memory source) public {
+        postRequestSrc = source;
+    }
+
+    function fulfillRequest(bytes32 requestId, bytes memory response, bytes memory err) internal override {
+        latestResponse = response;
+        latestError = err;
+        emit OCRResponse(requestId, response, err);
+    }
+
+    function aggregateCreditScore(address borrower, uint256[] memory creditScores) public {
+        FunctionsRequest.Request memory req;
+        req.initializeRequestForInlineJavaScript(postRequestSrc);
+        string[] memory args = new string[](3);
+        string memory creditScoresString = _arrayToString(creditScores);
+        string memory borrowerString = Strings.toHexString(uint256(uint160(borrower)), 20);
+        
+        args[0] = creditScoresString;
+        args[1] = borrowerString; 
+        req.setArgs(args);
+        latestRequestId = _sendRequest(req.encodeCBOR(), subscriptionId, 0, "");
+
+        for (uint256 i = 0; i < creditScores.length; i++) {
+            finalScore[borrower] += creditScores[i];
+        }
+    }
+
+    // Helper function to convert uint256 array to a comma-separated string
+    function _arrayToString(uint256[] memory arr) private pure returns (string memory) {
+        if (arr.length == 0) {
+            return "";
+        }
+        string memory str = Strings.toString(arr[0]);
+        for (uint256 i = 1; i < arr.length; i++) {
+            str = string(abi.encodePacked(str, ",", Strings.toString(arr[i])));
+        }
+        return str;
+    }
+}
--- src/StormBitVault.sol
+++ src/StormBitVault.sol
@@ -1,183 +1,181 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.21;
+// // SPDX-License-Identifier: UNLICENSED
+// pragma solidity ^0.8.21;
 
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {AccessManagedUpgradeable} from "@openzeppelin/contracts-upgradeable/access/manager/AccessManagedUpgradeable.sol";
-import {ERC4626Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC4626Upgradeable.sol";
-import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+// import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+// import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// import {AccessManagedUpgradeable} from "@openzeppelin/contracts-upgradeable/access/manager/AccessManagedUpgradeable.sol";
+// import {ERC4626Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC4626Upgradeable.sol";
+// import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+// import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 
-import {ERC721Agreement} from "./agreements/ERC721Agreement.sol";
-import {ERC20Agreement} from "./agreements/ERC20Agreement.sol";
-import {SimpleAgreement} from "./agreements/SimpleAgreement.sol";
-import {IAgreement} from "./interfaces/IAgreement.sol";
-import {IStormBitVault} from "./interfaces/IStormBitVault.sol";
+// import {ERC721Agreement} from "./agreements/ERC721Agreement.sol";
+// import {ERC20Agreement} from "./agreements/ERC20Agreement.sol";
+// import {SimpleAgreement} from "./agreements/SimpleAgreement.sol";
+// import {IAgreement} from "./interfaces/IAgreement.sol";
+// import {IStormBitVault} from "./interfaces/IStormBitVault.sol";
 
+// contract StormBitVault is IStormBitVault, AccessManagedUpgradeable, ERC4626Upgradeable, UUPSUpgradeable {
+//     IERC20 private _underlyingToken;
+//     uint256 public totalDeposits;
+//     uint256 public totalBorrowed;
 
+//     // /**
+//     // * @dev Agreement ERC20 strategy
+//     // */
+//     // ERC20Agreement public _ERC20_AGREEEMENT;
 
-contract StormBitVault is IStormBitVault, AccessManagedUpgradeable, ERC4626Upgradeable, UUPSUpgradeable {
-    IERC20 private _underlyingToken;
-    uint256 public totalDeposits;
-    uint256 public totalBorrowed;
+//     // /**
+//     // * @dev Agreement ERC721 strategy
+//     // */
+//     // ERC721Agreement public _ERC721_AGREEEMENT;
 
-    // /**
-    // * @dev Agreement ERC20 strategy
-    // */
-    // ERC20Agreement public _ERC20_AGREEEMENT;
+//     // /**
+//     // * @dev Agreement Simple strategy
+//     // */
+//     // SimpleAgreement public _SIMPLE_AGREEEMENT;
 
-    // /**
-    // * @dev Agreement ERC721 strategy
-    // */
-    // ERC721Agreement public _ERC721_AGREEEMENT;
+//     /**
+//      * @dev StormBit strategy
+//      */
+//     IAgreement internal immutable _STORMBIT_AGREEEMENT;
 
-    // /**
-    // * @dev Agreement Simple strategy
-    // */
-    // SimpleAgreement public _SIMPLE_AGREEEMENT;
+//     constructor(IERC20 _underlyingToken, IAgreement stormbitAgreement) payable {
+//         _underlyingToken = _underlyingToken;
+//         _STORMBIT_AGREEEMENT = stormbitAgreement;
+//         _disableInitializers();
+//     }
 
-    /**
-     * @dev StormBit strategy
-     */
-    IAgreement internal immutable _STORMBIT_AGREEEMENT;
+//     function initialize(address accessManager) external initializer {
+//         __AccessManaged_init(accessManager);
+//         __ERC4626_init(_underlyingToken);
+//         __ERC20_init("DAI", "DAI");
+//     }
 
-    constructor(IERC20 _underlyingToken, IAgreement stormbitAgreement) payable {
-        _underlyingToken = _underlyingToken;
-        _STORMBIT_AGREEEMENT = stormbitAgreement;
-        _disableInitializers();
-    }
+//     receive() external payable virtual {
+//         // TO DO : security to be implemented // @audit - protect against creating withdrawal from attackers
+//     }
 
-    function initialize(address accessManager) external initializer {
-        __AccessManaged_init(accessManager);
-        __ERC4626_init(_underlyingToken);
-        __ERC20_init("DAI", "DAI");
-    }
+//     /**
+//      * @inheritdoc ERC4626Upgradeable
+//      * @dev Restricted in this context is like `whenNotPaused` modifier from Pausable.sol
+//      */
+//     function deposit(uint256 assets, address receiver) public virtual override restricted returns (uint256) {
+//         return super.deposit(assets, receiver);
+//     }
 
-    receive() external payable virtual {
-        // TO DO : security to be implemented // @audit - protect against creating withdrawal from attackers
-    }
+//     /**
+//      * @inheritdoc ERC4626Upgradeable
+//      * @dev Restricted in this context is like `whenNotPaused` modifier from Pausable.sol
+//      */
+//     function mint(uint256 assets, address receiver) public override restricted returns (uint256) {
+//         return super.mint(assets, receiver);
+//     }
 
-    /**
-     * @inheritdoc ERC4626Upgradeable
-     * @dev Restricted in this context is like `whenNotPaused` modifier from Pausable.sol
-     */
-    function deposit(uint256 assets, address receiver) public virtual override restricted returns (uint256) {
-        return super.deposit(assets, receiver);
-    }
+//     /**
+//      * Not allowed
+//      */
+//     function redeem(uint256, address, address) public virtual override returns (bytes4) {
+//         revert WithdrawalsAreDisabled();
+//     }
 
-    /**
-     * @inheritdoc ERC4626Upgradeable
-     * @dev Restricted in this context is like `whenNotPaused` modifier from Pausable.sol
-     */
-    function mint(uint256 assets, address receiver) public override restricted returns (uint256) {
-        return super.mint(assets, receiver);
-    }
+//     /**
+//      * @notice Not allowed
+//      */
+//     function withdraw(uint256, address, address) public virtual override returns (bytes4) {
+//         revert WithdrawalsAreDisabled();
+//     }
 
-    /**
-     * Not allowed
-     */
-    function redeem(uint256, address, address) public virtual override returns (bytes4) {
-        revert WithdrawalsAreDisabled();
-    }
+//     /**
+//      * @dev See {IERC4626-totalAssets}.
+//      */
+//     function totalAssets() public view virtual override returns (uint256) {
+//         return _underlyingToken.balanceOf(address(this));
+//     }
 
-    /**
-     * @notice Not allowed
-     */
-    function withdraw(uint256, address, address) public virtual override returns (bytes4) {
-        revert WithdrawalsAreDisabled();
-    }
+//     /**
+//      * @notice Returns the number of decimals used to get its user representation.
+//      */
+//     function decimals() public pure override(ERC20Upgradeable, ERC4626Upgradeable) returns (uint8) {
+//         return 18;
+//     }
 
-    /**
-     * @dev See {IERC4626-totalAssets}.
-     */
-    function totalAssets() public view virtual override returns (uint256) {
-        return _underlyingToken.balanceOf(address(this));
-    }
+//     /**
+//      * @dev Authorizes an upgrade to a new implementation
+//      * Restricted access
+//      * @param newImplementation The address of the new implementation
+//      */
+//     // slither-disable-next-line dead-code
+//     function _authorizeUpgrade(address newImplementation) internal virtual override restricted {}
 
-    /**
-     * @notice Returns the number of decimals used to get its user representation.
-     */
-    function decimals() public pure override(ERC20Upgradeable, ERC4626Upgradeable) returns (uint8) {
-        return 18;
-    }
+//     // // Interest rate following Compound model
+//     // // Lenders earn interest based on the liquidity they provide.
+//     // // Borrowers pay interest on their loan.
+//     // // How is the spread calculated ? => Interet earned by the protocol
 
-    /**
-     * @dev Authorizes an upgrade to a new implementation
-     * Restricted access
-     * @param newImplementation The address of the new implementation
-     */
-    // slither-disable-next-line dead-code
-    function _authorizeUpgrade(address newImplementation) internal virtual override restricted {}
+//     // function deposit(uint256 _amount) external {
+//     //     _underlyingToken.transferFrom(msg.sender, address(this), _amount);
+//     //     totalDeposits += _amount;
+//     // }
 
-    // // Interest rate following Compound model
-    // // Lenders earn interest based on the liquidity they provide.
-    // // Borrowers pay interest on their loan.
-    // // How is the spread calculated ? => Interet earned by the protocol
+//     // function borrow(uint256 _amount) external {
+//     //     require(_underlyingToken.balanceOf(address(this)) >= _amount, "Not enough liquidity");
+//     //     _underlyingToken.transfer(msg.sender, _amount);
+//     //     totalBorrowed += _amount;
+//     // }
 
-    // function deposit(uint256 _amount) external {
-    //     _underlyingToken.transferFrom(msg.sender, address(this), _amount);
-    //     totalDeposits += _amount;
-    // }
+//     // /**
+//     //  * @notice Returns the utilization of the pool
+//     //  *     @dev utilization =  borrowed / total deposits
+//     //  */
+//     // function getUtilization() public view returns (uint256) {
+//     //     uint256 utilization = totalBorrowed / totalDeposits;
+//     //     return utilization;
+//     // }
 
-    // function borrow(uint256 _amount) external {
-    //     require(_underlyingToken.balanceOf(address(this)) >= _amount, "Not enough liquidity");
-    //     _underlyingToken.transfer(msg.sender, _amount);
-    //     totalBorrowed += _amount;
-    // }
+//     // /**
+//     //  * @notice returns intereste rates
+//     //  *     @dev  interest rates are calculated based on the utilization of the pool
+//     //  */
+//     // function getInterestRates() public view returns (uint256) {
+//     //     return 1;
+//     // }
 
-    // /**
-    //  * @notice Returns the utilization of the pool
-    //  *     @dev utilization =  borrowed / total deposits
-    //  */
-    // function getUtilization() public view returns (uint256) {
-    //     uint256 utilization = totalBorrowed / totalDeposits;
-    //     return utilization;
-    // }
+//     // /**
+//     //  * @notice returns the supply rate
+//     //  *     @dev  supply rate is interest rate for lenders
+//     //  */
+//     // function getSupplyRate() public view returns (uint256) {
+//     //     return getInterestRates() * getUtilization();
+//     // }
 
-    // /**
-    //  * @notice returns intereste rates
-    //  *     @dev  interest rates are calculated based on the utilization of the pool
-    //  */
-    // function getInterestRates() public view returns (uint256) {
-    //     return 1;
-    // }
+//     // // --------------- Liquidition management -------------------------- //
 
-    // /**
-    //  * @notice returns the supply rate
-    //  *     @dev  supply rate is interest rate for lenders
-    //  */
-    // function getSupplyRate() public view returns (uint256) {
-    //     return getInterestRates() * getUtilization();
-    // }
+//     // // TO DO : Protect Lenders from liquidation
 
-    // // --------------- Liquidition management -------------------------- //
+//     // //
+//     // struct Collateral {
+//     //     uint256 collateralType;
+//     // }
 
-    // // TO DO : Protect Lenders from liquidation
+//     // function getCollateralValue(Collateral memory _collateral, uint256 _DAIAmount) public view returns (uint256) {
+//     //     if (_collateral.collateralType == 1) {
+//     //         return _DAIAmount;
+//     //     }
+//     //     // } else {
+//     //     //     // TO DO : use API to price the NFT
+//     //     // }
 
-    // //
-    // struct Collateral {
-    //     uint256 collateralType;
-    // }
+//     //     return _DAIAmount;
+//     // }
 
-    // function getCollateralValue(Collateral memory _collateral, uint256 _DAIAmount) public view returns (uint256) {
-    //     if (_collateral.collateralType == 1) {
-    //         return _DAIAmount;
-    //     }
-    //     // } else {
-    //     //     // TO DO : use API to price the NFT
-    //     // }
+//     // function getCollateralFactor(uint256 _loanValue) public view returns (uint256) {
+//     //     Collateral memory _collateral = Collateral(1);
+//     //     return _loanValue / getCollateralValue(_collateral, 20000);
+//     // }
 
-    //     return _DAIAmount;
-    // }
-
-    // function getCollateralFactor(uint256 _loanValue) public view returns (uint256) {
-    //     Collateral memory _collateral = Collateral(1);
-    //     return _loanValue / getCollateralValue(_collateral, 20000);
-    // }
-
-    // /**
-    //  * @notice returns the liquidation factor
-    //  * @dev  liquidation factor is the ratio of the value of the collateral to the value of the loan
-    //  */
-    // function getLiquidationFactor() public view returns (uint256) {}
-}
+//     // /**
+//     //  * @notice returns the liquidation factor
+//     //  * @dev  liquidation factor is the ratio of the value of the collateral to the value of the loan
+//     //  */
+//     // function getLiquidationFactor() public view returns (uint256) {}
+// }
